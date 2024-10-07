5f6d3a1d5926f38e90d6fd08f050912897fc36d8
Caelum1121 <s4073761@rmit.edu.vn>
2024-10-08 00:42:09 +0700

    Update

--- js/main.js
+++ js/main.js
@@ -65,4 +65,63 @@
 })(jQuery);
 
 
+const addEventOnElem = function (elem, type, callback) {
+  if (elem.length > 1) {
+    for (let i = 0; i < elem.length; i++) {
+      elem[i].addEventListener(type, callback);
+    }
+  } else {
+    elem.addEventListener(type, callback);
+  }
+}
+
+
+const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-back-top-btn]");
+
+const headerActive = function () {
+  if (window.scrollY > 150) {
+    header.classList.add("active");
+    backTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    backTopBtn.classList.remove("active");
+  }
+};
+
+// Add event listener for scroll
+window.addEventListener("scroll", headerActive);
+
+let lastScrolledPos = 0;
+
+const headerSticky = function () {
+  if (lastScrolledPos >= window.scrollY) {
+    header.classList.remove("header-hide");
+  } else {
+    header.classList.add("header-hide");
+  }
+
+  lastScrolledPos = window.scrollY;
+};
+
+// Add event listener for scroll
+window.addEventListener("scroll", headerSticky);
+
+/**
+ * scroll reveal effect
+ */
+
+const sections = document.querySelectorAll("[data-section]");
+
+const scrollReveal = function () {
+  for (let i = 0; i < sections.length; i++) {
+    if (sections[i].getBoundingClientRect().top < window.innerHeight / 2) {
+      sections[i].classList.add("active");
+    }
+  }
+};
+
+// Trigger the scroll reveal effect initially and on scroll
+scrollReveal();
+window.addEventListener("scroll", scrollReveal);
 
